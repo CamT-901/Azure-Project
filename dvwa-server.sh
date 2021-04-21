12ca8b90985fe88dd92f5e725b23a7716d9c1c03
Cam Todd <77467559+CamT-901@users.noreply.github.com>
2021-04-21 12:51:55 -0500

    Add files via upload

---
+++ dvwa-server.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if ! [ $(id -u) = 0 ]; then
+	echo "The script need to be run as root." >&2
+	exit 1
+fi
+if [ $SUDO_USER ]; then
+	real_user=$SUDO_USER
+else
+	real_user=$(whoami)
+fi
+apt-get update
+apt-get upggrade
+apt install docker.io
+apt install python3-pip
+apt install docker
+systemctl start docker
+docker pull vulnerables/web-dvwa
+docker run -it -p 80:80 --restart always vulnerables/web-dvwa
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
